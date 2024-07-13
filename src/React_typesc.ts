53db783e7221e52f18c54da0281166f2973a3d08
任意のなめこ <worktask1124@outlook.com>
2024-07-13 22:07:24 +0900

    first commit

---
+++ src/React_typesc.ts
@@ -0,0 +1,27 @@
+import { flushSync } from "react-dom"
+
+const Hawai:React.FC=()=>{
+    return 
+}
+
+const ClassGetObject="null";
+
+const GetObjectDomain="juniper";
+
+const Webrepository=()=>{
+
+for(let i=0;i<100;i++){
+    
+    if(ClassGetObject=="null"){
+console.error();
+    }else{
+GetObjectDomain.split("u");
+    }
+
+ClassGetObject.split(GetObjectDomain);
+}
+
+
+
+    return
+}
